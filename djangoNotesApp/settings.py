af385f8078b40974ae7f1e3bf7102320199f6cca
sombromar <sombromar@gmail.com>
2021-07-14 17:14:08 -0300

    no se arreglo ni chota

--- djangoNotesApp/settings.py
+++ djangoNotesApp/settings.py
@@ -155,7 +155,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = (
-    BASE_DIR / 'staticfiles/static',
+    STATIC_ROOT / 'static',
 )
 
 STATICFILES_FINDERS = [
